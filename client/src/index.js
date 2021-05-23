cc20cc37c53be499085a73c04618d194500c3462
Ishai Yaish <ishaiyaish@gmail.com>
2021-05-23 11:33:57 +0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,7 @@ import reducers from "./reducers/index";
 
 const enhancers = [
   applyMiddleware(reduxThunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 ];
 
 const store = createStore(reducers, compose(...enhancers));
